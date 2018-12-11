fb2b111aa6f37da26a6732e29eb6df454bf2e873
Matt J (Programmer) <mj209069@falmouth.ac.uk>
2018-12-11 13:32:42 +0000

    Added a main menu to the game as well as a cutscene scene. Reorganised the shader showcase scene. Removed depreciated script SpinToWin.cs

---
+++ Assets/Scripts/Management/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+
+    public void StartGame(bool doCutscene)
+    {
+        if (doCutscene)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            SceneManager.LoadScene(2);
+        }
+
+    }
+
+    public void LoadShaderShowcase()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
