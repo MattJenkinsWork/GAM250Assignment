483ab7ba3540ca72497ce26dac729a0f218741f5
Matt J (Programmer) <mj209069@falmouth.ac.uk>
2018-12-13 02:46:19 +0000

    Added a word document to read from during viva. Also created cutscene materials and scripts and updated my notes

---
+++ Assets/Scripts/Cutscene/CutsceneManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CutsceneManager : MonoBehaviour {
+
+    public string[] dialogue;
+
+    int currentLine = 0;
+
+    public Text textbox;
+    public GameObject textboxImage;
+
+    private void Start()
+    {
+        NextLine();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+        if (Input.anyKeyDown)
+        {
+            currentLine++;
+
+            textboxImage.SetActive(currentLine >= 1);
+
+            //if (currentline >= 1)
+            //    textboximage.setactive(true);
+            //else
+            //    textboximage.setactive(false);
+
+
+            if (currentLine >= dialogue.Length)
+                SceneManager.LoadScene(2);
+            else
+                NextLine();
+        }
+
+
+	}
+    
+    void NextLine()
+    {
+        
+        textbox.text = dialogue[currentLine];
+
+    }
+
+}
---
+++ Assets/Scripts/Cutscene/JiggleSlightly.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JiggleSlightly : MonoBehaviour {
+
+bool up;
+int interval;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (interval == 10) {
+			if (!up) {
+				transform.Translate (new Vector3 (0, 0.001f, 0));
+				interval = Random.Range (0, 10);
+				up = !up;
+			} else {
+				transform.Translate (new Vector3 (0, -0.001f, 0));
+				interval = Random.Range (0, 10);
+				up = !up;
+			}
+		}
+
+		interval++;
+
+
+	}
+}
